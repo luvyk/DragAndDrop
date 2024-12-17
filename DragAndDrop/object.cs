89c1593269b892ef52fc941e38f80ef2ee2c45c6
VytiskLukas <VytiskLukas@L107PC02>
2024-12-17 14:25:17 +0100

    push

---
+++ DragAndDrop/object.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragAndDrop
+{
+    public class @object
+    {
+        public string nazev { get; set; }
+        public 
+    }
+}
